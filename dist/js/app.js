61369eb9c8197b2cb67832003063b2ae8f894705
Dru Kelly <drukelly@gmail.com>
2019-05-03 21:36:27 -0700

    Add LIRI-Bot to homepage

--- dist/js/app.js
+++ dist/js/app.js
@@ -14,24 +14,98 @@ function formatDate(date) {
 }
 
 const recentWorks = [
-  { url: "http://labs.vmware.com/flings", year: 2015, title: "Flings", description: "Icon Design", slug: "vmware-flings" },
-  { url: "http://vmware.github.io", year: 2017, title: "VMware GitHub", description: "AngularJS, Front End Development", slug: "vmware-github" },
-  { url: "javascript:;", year: 2016, title: "Office of the CTO", description: "Branding and Logo Design", slug: "vmware-octo" },
-  { url: "javascript:;", year: 2015, title: "RADIO 2015", description: "Theme, Site Design and Development", slug: "vmware-radio" },
-  { url: "javascript:;", year: 2015, title: "RADIO Merch", description: "Event Merch", slug: "vmware-radio-merch" },
-  { url: "javascript:;", year: 2015, title: "RADIO Concept", description: "Brand Concept", slug: "vmware-radio-concept" },
-  { url: "javascript:;", year: 2016, title: "RADIO 2016", description: "Theme, Site Design and Development", slug: "vmware-radio" },
-  { url: "javascript:;", year: 2014, title: "Borathon", description: "Poster Design", slug: "vmware-borathon" },
-  { url: "/video-drumkit", year: 2017, title: "Video Drumkit", description: "Inspiration from #javascript30", slug: "video-drumkit" },
-  { url: "https://www.ralphandmaria.com", year: 2018, title: "Ralph and Maria", description: "RSVP Form/Wedding site", slug: "ralph-maria" },
-  { url: "javascript:;", year: 2014, title: "Team Lunch", description: "Poster Design", slug: "vmware-team-lunch" }
+  {
+    url: "http://labs.vmware.com/flings",
+    year: 2015,
+    title: "Flings",
+    description: "Icon Design",
+    slug: "vmware-flings"
+  },
+  {
+    url: "http://vmware.github.io",
+    year: 2017,
+    title: "VMware GitHub",
+    description: "AngularJS, Front End Development",
+    slug: "vmware-github"
+  },
+  {
+    url: "javascript:;",
+    year: 2016,
+    title: "Office of the CTO",
+    description: "Branding and Logo Design",
+    slug: "vmware-octo"
+  },
+  {
+    url: "javascript:;",
+    year: 2015,
+    title: "RADIO 2015",
+    description: "Theme, Site Design and Development",
+    slug: "vmware-radio"
+  },
+  {
+    url: "javascript:;",
+    year: 2015,
+    title: "RADIO Merch",
+    description: "Event Merch",
+    slug: "vmware-radio-merch"
+  },
+  {
+    url: "javascript:;",
+    year: 2015,
+    title: "RADIO Concept",
+    description: "Brand Concept",
+    slug: "vmware-radio-concept"
+  },
+  {
+    url: "javascript:;",
+    year: 2016,
+    title: "RADIO 2016",
+    description: "Theme, Site Design and Development",
+    slug: "vmware-radio"
+  },
+  {
+    url: "javascript:;",
+    year: 2014,
+    title: "Borathon",
+    description: "Poster Design",
+    slug: "vmware-borathon"
+  },
+  {
+    url: "/video-drumkit",
+    year: 2017,
+    title: "Video Drumkit",
+    description: "Inspiration from #javascript30",
+    slug: "video-drumkit"
+  },
+  {
+    url: "https://www.ralphandmaria.com",
+    year: 2018,
+    title: "Ralph and Maria",
+    description: "RSVP Form/Wedding site",
+    slug: "ralph-maria"
+  },
+  {
+    url: "javascript:;",
+    year: 2014,
+    title: "Team Lunch",
+    description: "Poster Design",
+    slug: "vmware-team-lunch"
+  },
+  {
+    url: "https://github.com/drukelly/LIRI-Bot",
+    year: 2019,
+    title: "LIRI Bot",
+    description: "Node.js command line app",
+    slug: "liri-bot",
+    format: "png"
+  }
 ];
 
 const newest = recentWorks.sort((a, b) => a.year > b.year ? -1 : 1);
 const target = document.querySelector('.recent-works');
 newest.forEach(work =>
   target.innerHTML+= `<div class="work-item fl w-50 w-25-ns" ontouchstart="this.classList.toggle('hover');"><a href="${work.url}" target="_blank" class="db aspect-ratio aspect-ratio--1x1">
-    <div role="img" aria-label="" style="background-image:url(/img/${work.slug}-${work.year}.jpg);" class="front bg-center cover aspect-ratio--object"></div>
+    <div role="img" aria-label="" style="background-image:url(/img/${work.slug}-${work.year}.${work.format ? "png" : "jpg"});" class="front bg-center cover aspect-ratio--object"></div>
     <div class="back bg-yellow w-100">
       <figure class="ma0 tc">
         <figcaption class="ma0 near-black">
