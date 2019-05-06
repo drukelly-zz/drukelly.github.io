10e4c5452e782ad0fa7180f8e158b8e8e7083211
Dru Kelly <drukelly@gmail.com>
2019-05-06 10:59:12 -0700

    Add Train Scheduler

--- dist/js/app.js
+++ dist/js/app.js
@@ -29,42 +29,36 @@ const recentWorks = [
     slug: "vmware-github"
   },
   {
-    url: "javascript:;",
     year: 2016,
     title: "Office of the CTO",
     description: "Branding and Logo Design",
     slug: "vmware-octo"
   },
   {
-    url: "javascript:;",
     year: 2015,
     title: "RADIO 2015",
     description: "Theme, Site Design and Development",
     slug: "vmware-radio"
   },
   {
-    url: "javascript:;",
     year: 2015,
     title: "RADIO Merch",
     description: "Event Merch",
     slug: "vmware-radio-merch"
   },
   {
-    url: "javascript:;",
     year: 2015,
     title: "RADIO Concept",
     description: "Brand Concept",
     slug: "vmware-radio-concept"
   },
   {
-    url: "javascript:;",
     year: 2016,
     title: "RADIO 2016",
     description: "Theme, Site Design and Development",
     slug: "vmware-radio"
   },
   {
-    url: "javascript:;",
     year: 2014,
     title: "Borathon",
     description: "Poster Design",
@@ -85,7 +79,6 @@ const recentWorks = [
     slug: "ralph-maria"
   },
   {
-    url: "javascript:;",
     year: 2014,
     title: "Team Lunch",
     description: "Poster Design",
@@ -98,15 +91,23 @@ const recentWorks = [
     description: "Node.js command line app",
     slug: "liri-bot",
     format: "png"
+  },
+  {
+    url: "https://www.drukelly.com/Train-Scheduler",
+    year: 2019,
+    title: "Train Scheduler",
+    description: "Firebase + Moment.js",
+    slug: "train-scheduler",
+    format: "png"
   }
 ];
 
 const newest = recentWorks.sort((a, b) => a.year > b.year ? -1 : 1);
 const target = document.querySelector('.recent-works');
 newest.forEach(work =>
-  target.innerHTML+= `<div class="work-item fl w-50 w-25-ns" ontouchstart="this.classList.toggle('hover');"><a href="${work.url}" target="_blank" class="db aspect-ratio aspect-ratio--1x1">
+  target.innerHTML+= `<div class="work-item fl w-50 w-25-ns" ontouchstart="this.classList.toggle('hover');"><a ${work.url ? `href=${work.url} target="_blank" ` : "" } class="db aspect-ratio aspect-ratio--1x1 pointer">
     <div role="img" aria-label="" style="background-image:url(/img/${work.slug}-${work.year}.${work.format ? "png" : "jpg"});" class="front bg-center cover aspect-ratio--object"></div>
-    <div class="back bg-yellow w-100">
+    <div class="back bg-light-blue w-100">
       <figure class="ma0 tc">
         <figcaption class="ma0 near-black">
           <h3 class="f6 f4-ns ma0 lh-title-ns">${work.title}</h3>
